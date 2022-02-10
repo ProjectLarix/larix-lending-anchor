93398b2e159d42e470bf0e55f5cdce037337bb09
li zengping <lizengping93@gmail.com>
2022-02-10 20:53:07 +0800

    Add some cpi instructions

--- src/accounts.rs
+++ src/accounts.rs
@@ -17,3 +17,56 @@ pub struct RefreshReserve<'info> {
     pub reserve:AccountInfo<'info>,
     pub oracle:AccountInfo<'info>,
 }
+#[derive(Accounts)]
+pub struct DepositReserveLiquidity<'info> {
+    #[account(mut)]
+    pub source_liquidity_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub destination_collateral_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_collateral_mint_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_liquidity_supply_pubkey: AccountInfo<'info>,
+    pub lending_market_pubkey: AccountInfo<'info>,
+    pub user_transfer_authority_pubkey: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct RedeemReserveCollateral<'info> {
+    #[account(mut)]
+    pub source_collateral_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub destination_liquidity_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_collateral_mint_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub reserve_liquidity_supply_pubkey: AccountInfo<'info>,
+    pub lending_market_pubkey: AccountInfo<'info>,
+    pub user_transfer_authority_pubkey: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct InitObligation<'info>{
+    #[account(mut)]
+    pub obligation_pubkey: AccountInfo<'info>,
+    pub lending_market_pubkey: AccountInfo<'info>,
+    pub obligation_owner_pubkey: AccountInfo<'info>,
+}
+#[derive(Accounts)]
+pub struct InitObligation2<'info>{
+    #[account(mut)]
+    pub obligation_pubkey: AccountInfo<'info>,
+    pub lending_market_pubkey: AccountInfo<'info>,
+    pub obligation_owner_pubkey: AccountInfo<'info>,
+    #[account(mut)]
+    pub payer:Signer<'info>,
+    pub system_program:AccountInfo<'info>
+}
+#[derive(Accounts)]
+pub struct RefreshObligation<'info>{
+    #[account(mut)]
+    pub obligation:AccountInfo<'info>,
+    pub reserves:AccountInfo<'info>
+}
--- src/instructions.rs
+++ src/instructions.rs
@@ -4,7 +4,8 @@ use anchor_lang::solana_program::instruction::Instruction;
 use anchor_lang::solana_program::program::{invoke, invoke_signed};
 use larix_lending::id as larix_lending_id;
 use larix_lending::instruction::LendingInstruction;
-use crate::accounts::{InitLendingMarket, RefreshReserve};
+use larix_lending::state::obligation::OBLIGATION_LEN;
+use crate::accounts::{DepositReserveLiquidity, InitLendingMarket, InitObligation, InitObligation2, RedeemReserveCollateral, RefreshReserve};
 
 pub fn init_lending_market<'a, 'b, 'c, 'info>(
     ctx: CpiContext<'a, 'b, 'c, 'info, InitLendingMarket<'info>>,
@@ -53,4 +54,91 @@ pub fn refresh_reserve<'a, 'b, 'c, 'info>(
         vec![ctx.accounts.oracle.key()],
     );
     invoke(&ix, &ctx.to_account_infos())
+}
+pub fn deposit_reserve_liquidity<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, DepositReserveLiquidity<'info>>,
+    liquidity_amount:u64,
+) -> ProgramResult{
+    let ix = larix_lending::instruction::deposit_reserve_liquidity(
+        larix_lending_id(),
+        liquidity_amount,
+        ctx.accounts.source_liquidity_pubkey.key(),
+        ctx.accounts.destination_collateral_pubkey.key(),
+        ctx.accounts.reserve_pubkey.key(),
+        ctx.accounts.reserve_collateral_mint_pubkey.key(),
+        ctx.accounts.reserve_liquidity_supply_pubkey.key(),
+        ctx.accounts.lending_market_pubkey.key(),
+        ctx.accounts.user_transfer_authority_pubkey.key()
+    );
+    invoke_signed(&ix, &ctx.to_account_infos(),ctx.signer_seeds)
+}
+pub fn redeem_reserve_collateral<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, RedeemReserveCollateral<'info>>,
+    liquidity_amount:u64,
+) -> ProgramResult{
+    let ix = larix_lending::instruction::redeem_reserve_collateral(
+        larix_lending_id(),
+        liquidity_amount,
+        ctx.accounts.source_collateral_pubkey.key(),
+        ctx.accounts.destination_liquidity_pubkey.key(),
+        ctx.accounts.reserve_pubkey.key(),
+        ctx.accounts.reserve_collateral_mint_pubkey.key(),
+        ctx.accounts.reserve_liquidity_supply_pubkey.key(),
+        ctx.accounts.lending_market_pubkey.key(),
+        ctx.accounts.user_transfer_authority_pubkey.key()
+    );
+    invoke_signed(&ix, &ctx.to_account_infos(),ctx.signer_seeds)
+}
+
+pub fn init_obligation<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, InitObligation<'info>>,
+) -> ProgramResult {
+    let ix = larix_lending::instruction::init_obligation(
+        larix_lending_id(),
+        ctx.accounts.obligation_pubkey.key(),
+        ctx.accounts.lending_market_pubkey.key(),
+        ctx.accounts.obligation_owner_pubkey.key()
+    );
+    invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.obligation_pubkey.to_account_info(),
+            ctx.accounts.lending_market_pubkey.to_account_info(),
+            ctx.accounts.obligation_owner_pubkey.to_account_info(),
+        ],
+        ctx.signer_seeds
+    )
+}
+pub fn init_obligation2<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, InitObligation2<'info>>,
+) -> ProgramResult {
+    let min_balance = Rent::get()?.minimum_balance(OBLIGATION_LEN);
+    let ix = anchor_lang::solana_program::system_instruction::transfer(
+        &ctx.accounts.payer.key(),
+        &ctx.accounts.obligation_pubkey.key(),
+        min_balance
+    );
+    invoke_signed(
+        &ix,
+        &[
+                      ctx.accounts.system_program.to_account_info(),
+                      ctx.accounts.payer.to_account_info(),
+                      ctx.accounts.obligation_pubkey.to_account_info(),
+                  ],
+        ctx.signer_seeds)?;
+    let ix = larix_lending::instruction::init_obligation(
+        larix_lending_id(),
+        ctx.accounts.obligation_pubkey.key(),
+        ctx.accounts.lending_market_pubkey.key(),
+        ctx.accounts.obligation_owner_pubkey.key()
+    );
+    invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.obligation_pubkey.to_account_info(),
+            ctx.accounts.lending_market_pubkey.to_account_info(),
+            ctx.accounts.obligation_owner_pubkey.to_account_info(),
+        ],
+        ctx.signer_seeds
+    )
 }
